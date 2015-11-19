dbdafb4ef9ebafa1d023b14289cd4c00f304c699
Dsljlbaby <dsljlbaby@gmail.com>
2015-11-19 18:44:53 +0800

    query weather

---
+++ weather.py
@@ -0,0 +1,30 @@
+# -*- coding: cp936 -*-
+import urllib2
+import json
+from city import city
+exit=False
+while not exit:
+   cityname = raw_input('你想查哪个城市的天气？（输入q结束查询）\n')
+   if cityname=="q" or cityname=="Q":
+        print('本次查询结束，欢迎下次使用！!')
+        exit=True
+   else:
+       citycode = city.get(cityname)
+       if citycode:
+             try:
+                url = ('http://www.weather.com.cn/data/cityinfo/%s.html'
+                        % citycode)
+                content = urllib2.urlopen(url).read()
+                data = json.loads(content)
+                result = data['weatherinfo']
+                str_temp = ('%s\n%s ~ %s') % (
+                     result['weather'],
+                     result['temp1'],
+                     result['temp2']
+                 )
+                print str_temp
+             except:
+                print '查询失败'
+       else:
+           print '没有找到该城市'
+
